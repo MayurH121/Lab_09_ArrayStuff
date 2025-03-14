82e6f9311b1a473648c062fb9c4d96294207d591
Mayur <mayurharisinghani@gmail.com>
2025-03-14 07:11:51 -0600

    Task 3 Complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] dataPoints = new int[100];
+
+        // Task 2
+        Random rnd = new Random();
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            dataPoints[i] = rnd.nextInt(100) + 1; // Random value between 1 and 100
+        }
+
+        // Task 3
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            System.out.print(dataPoints[i]);
+            if (i < dataPoints.length - 1)
+            {
+                System.out.print(" | ");
+            }
+        }
+        System.out.println();
     }
 }
